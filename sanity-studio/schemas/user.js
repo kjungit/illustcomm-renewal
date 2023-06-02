10be8c68f878a1535f28842e03e26074a23af07e
kjun <kjun_all@naver.com>
2023-06-03 04:24:29 +0900

    conf: sanity-studio 셋업

---
+++ sanity-studio/schemas/user.js
@@ -0,0 +1,78 @@
+export default {
+  title: 'User',
+  name: 'user',
+  type: 'document',
+  fields: [
+    {
+      title: 'Username', // 스튜디오 UI에서 보는 이름
+      name: 'username', // 데이터상의 이름
+      type: 'string',
+    },
+    {
+      title: 'Name',
+      name: 'name',
+      type: 'string',
+    },
+    {
+      title: 'Email',
+      name: 'email',
+      type: 'string',
+    },
+    {
+      title: 'Profile',
+      name: 'profile',
+      type: 'image',
+    },
+    {
+      title: 'Following',
+      name: 'following',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: [
+            {
+              type: 'user',
+            },
+          ],
+        },
+      ],
+      validation: (Rule) => Rule.unique(),
+    },
+    {
+      title: 'Followers',
+      name: 'followers',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: [
+            {
+              type: 'user',
+            },
+          ],
+        },
+      ],
+      validation: (Rule) => Rule.unique(),
+    },
+    {
+      title: 'Bookmarks',
+      name: 'bookmarks',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: [{type: 'post'}],
+        },
+      ],
+      validation: (Rule) => Rule.unique(),
+    },
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      subtitle: 'username',
+      media: 'profile',
+    },
+  },
+}
