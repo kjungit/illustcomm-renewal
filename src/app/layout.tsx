26e0d1dacee1d0943e6c77cdf8041997e6cdc4d6
kjun <kjun_all@naver.com>
2023-07-22 21:55:39 +0900

    Feat: 헤더 추가 및 스타일 작업

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/Navbar";
 import "./globals.css";
 import { Open_Sans } from "next/font/google";
 
@@ -14,8 +15,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={openSans.className}>{children}</body>
+    <html lang="en" className={openSans.className}>
+      <body className="w-full max-w-screen-xl mx-auto overflow-auto ">
+        <header className="sticky top-0 z-10 bg-white border-b">
+          <Navbar />
+        </header>
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
