391cd33793cc978762754957036f31dd12894802
kjun <kjun_all@naver.com>
2023-07-25 03:24:59 +0900

    Feat: OAuth 관련 세팅 - SessionProvider 설정 - createClient설정 후 sanity데이터 연결

---
+++ src/service/user.ts
@@ -0,0 +1,24 @@
+import { client } from "./sanity";
+
+type OAuthUser = {
+  id: string;
+  email: string;
+  name: string;
+  username: string;
+  image?: string | null;
+};
+
+export async function addUser({ id, username, email, name, image }: OAuthUser) {
+  console.log(id, username, email, name, image);
+  return client.createIfNotExists({
+    _id: id,
+    _type: "user",
+    username,
+    email,
+    name,
+    image,
+    following: [],
+    followers: [],
+    bookmarks: [],
+  });
+}
