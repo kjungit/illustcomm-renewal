9b2140c0fd8a508d63afe7288d5537971ee0064c
kjun <kjun_all@naver.com>
2023-07-25 03:16:36 +0900

    Feat: austOptions 정의, OAuth provider 연결

---
+++ src/lib/auth.ts
@@ -0,0 +1,51 @@
+import { addUser } from "@/service/user";
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import KakaoProvider from "next-auth/providers/kakao";
+import NaverProvider from "next-auth/providers/naver";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_OAUTH_ID || "",
+      clientSecret: process.env.GOOGLE_OAUTH_SECRET || "",
+    }),
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID || "",
+      clientSecret: process.env.KAKAO_CLIENT_SECRET || "",
+    }),
+    NaverProvider({
+      clientId: process.env.NAVER_CLIENT_ID || "",
+      clientSecret: process.env.NAVER_CLIENT_SECRET || "",
+    }),
+  ],
+  callbacks: {
+    async signIn({ user: { id, name, email, image } }) {
+      if (!email) {
+        return false;
+      }
+      addUser({
+        id,
+        name: name || "",
+        email,
+        image,
+        username: email?.split("@")[0],
+      });
+      return true;
+    },
+    async session({ session }) {
+      const user = session?.user;
+      console.log(session);
+      if (user) {
+        session.user = {
+          ...user,
+          username: user.email?.split("@")[0] || "",
+        };
+      }
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/auth/signin",
+  },
+};
