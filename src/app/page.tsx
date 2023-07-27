7ca11e4eb51a60ee0ba0bec5a77362cbfb840084
kjun <kjun_all@naver.com>
2023-07-27 21:09:20 +0900

    Feat: 메인페이지 컴포넌트 연결

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
-export default function Home() {
-  return <h1 className="text-red-900">illustcomm</h1>;
+import FollowingBar from "@/components/FollowingBar";
+import PostList from "@/components/PostList";
+import SideBar from "@/components/SideBar";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
+export default async function HomePage() {
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+  if (!user) {
+    redirect("/auth/signin");
+  }
+  return (
+    <section className="flex flex-col w-full max-w-full p-4 md:flex-row">
+      <div className="w-3/4">
+        <FollowingBar />
+        <PostList />
+      </div>
+      <div className="w-1/4 ml-8">
+        <SideBar user={user} />
+      </div>
+    </section>
+  );
 }
