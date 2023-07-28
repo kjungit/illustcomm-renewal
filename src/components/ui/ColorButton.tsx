15539b3d2ee6e86f3e8d17260becb9ed9a880a7f
kjun <kjun_all@naver.com>
2023-07-29 05:17:03 +0900

    UI: darkmode 환경 style 수정

--- src/components/ui/ColorButton.tsx
+++ src/components/ui/ColorButton.tsx
@@ -9,7 +9,7 @@ export default function ColorButton({ text, onClick }: Props) {
   return (
     <div className="p-[0.15rem] rounded-md bg-gradient-to-bl from-gray-600 via-gray-300 to-gray-100 ">
       <button
-        className="p-1 font-medium transition-opacity bg-white rounded-md hover:opacity-80"
+        className="p-1 font-medium transition-opacity bg-white rounded-md hover:opacity-80 dark:text-black"
         onClick={onClick}
       >
         {text}
