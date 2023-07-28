d2f983f98258d3be2841e601c8f6d70801c0bb21
kjun <kjun_all@naver.com>
2023-07-29 05:07:41 +0900

    Feat: 아이콘 ui컴포넌트 추가

---
+++ src/components/ui/icons/DarkModeIcon.tsx
@@ -0,0 +1,5 @@
+import { PiLightbulbFill } from "react-icons/pi";
+
+export default function DarkModeIcon() {
+  return <PiLightbulbFill className="text-black w-7 h-7" />;
+}
---
+++ src/components/ui/icons/LightModeIcon.tsx
@@ -0,0 +1,5 @@
+import { PiLightbulbFill } from "react-icons/pi";
+
+export default function LightModeIcon() {
+  return <PiLightbulbFill className="w-7 h-7" />;
+}
