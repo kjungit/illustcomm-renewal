bc0baf127a2ac2277c49fe6275d229094accd456
kjun <kjun_all@naver.com>
2023-07-31 03:44:43 +0900

    Feat: comment form 컴포넌트

--- src/components/CommentForm.tsx
+++ src/components/CommentForm.tsx
@@ -1,12 +1,19 @@
-export default function CommentForm() {
+import Avatar from "./Avatar";
+
+type Props = {
+  image: string;
+};
+
+export default function CommentForm({ image }: Props) {
   return (
-    <form className="flex items-center px-3 border-t border-neutral-300">
+    <form className="flex items-center border-t border-neutral-300">
+      {image && <Avatar image={image} size="xsmall" highlight />}
       <input
         className="w-full p-3 ml-2 border-none outline-none"
         type="text"
         placeholder="Add a comment..."
       />
-      <button className="ml-2 font-bold text-blue-700">Post</button>
+      <button className="ml-2 font-bold">Post</button>
     </form>
   );
 }
