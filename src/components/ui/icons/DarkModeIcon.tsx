27734cd6f4ed31c9af3f83a51fb386f38ae13ab5
kjun <kjun_all@naver.com>
2023-07-31 03:40:13 +0900

    Feat: 아이콘 수정

--- src/components/ui/icons/DarkModeIcon.tsx
+++ src/components/ui/icons/DarkModeIcon.tsx
@@ -1,5 +1,5 @@
 import { PiLightbulbFill } from "react-icons/pi";
 
 export default function DarkModeIcon() {
-  return <PiLightbulbFill className="text-black w-7 h-7" />;
+  return <PiLightbulbFill className=" w-7 h-7" />;
 }
