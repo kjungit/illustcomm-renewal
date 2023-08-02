f10b70fe9798bb67521bef8bde3a3f13009c5f13
kjun <kjun_all@naver.com>
2023-08-03 02:21:10 +0900

    Feat: user 페이지 UserPosts 컴포넌트 구현

---
+++ src/components/UserPosts.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { ProfileUser } from "@/model/user";
+import React, { useState } from "react";
+import PostIcon from "./ui/icons/PostIcon";
+import BookmarkIcon from "./ui/icons/BookmarkIcon";
+import HeartIcon from "./ui/icons/HeartIcon";
+import PostGrid from "./PostGrid";
+type Props = {
+  user: ProfileUser;
+};
+
+const tabs = [
+  { type: "posts", icon: <PostIcon /> },
+  { type: "saved", icon: <BookmarkIcon className="w-3 h-3" /> },
+  { type: "liked", icon: <HeartIcon className="w-3 h-3" /> },
+];
+
+export default function UserPosts({ user: { username } }: Props) {
+  const [query, setQuery] = useState(tabs[0].type);
+
+  return (
+    <section>
+      <ul className="flex justify-center uppercase">
+        {tabs.map(({ type, icon }) => (
+          <li
+            className={`p-4 mx-12 cursor-pointer border-black dark:border-white ${
+              type === query && "font-bold border-t-2"
+            }`}
+            key={type}
+            onClick={() => setQuery(type)}
+          >
+            <button className="mr-2 scale-150 md:scale-120">{icon}</button>
+            <span className="hidden md:inline">{type}</span>
+          </li>
+        ))}
+      </ul>
+      <PostGrid username={username} query={query} />
+    </section>
+  );
+}
