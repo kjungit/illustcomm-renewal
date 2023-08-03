d28f85bf8a99d4a4947212139e9be426a1e6ca0c
kjun <kjun_all@naver.com>
2023-08-04 03:46:19 +0900

    Feat: metadata 설정

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,5 +1,13 @@
 import UserSearch from "@/components/UserSearch";
+import { Metadata } from "next";
 import React from "react";
+
+export const dynamic = "force-dynamic";
+
+export const metadata: Metadata = {
+  title: "유저 검색",
+  description: "유저를 검색해보세요.",
+};
 
 export default function SearchPage() {
   return (
