2baddc3818aa56facfa21bd9a06be17a75d43725
kjun <kjun_all@naver.com>
2023-08-04 03:55:52 +0900

    Feat: metadata 설정 및 getUser cache 설정

--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -1,16 +1,19 @@
 import { getUserForProfile } from "@/service/user";
-import React from "react";
+import React, { cache } from "react";
 import { notFound } from "next/navigation";
 import UserProfile from "@/components/UserProfile";
 import UserPosts from "@/components/UserPosts";
+import { Metadata } from "next";
 type Props = {
   params: {
     username: string;
   };
 };
 
+const getUser = cache(async (username: string) => getUserForProfile(username));
+
 export default async function UserPage({ params: { username } }: Props) {
-  const user = await getUserForProfile(username);
+  const user = await getUser(username);
   if (!user) {
     notFound();
   }
@@ -20,4 +23,14 @@ export default async function UserPage({ params: { username } }: Props) {
       <UserPosts user={user} />
     </section>
   );
+}
+
+export async function generateMetadata({
+  params: { username },
+}: Props): Promise<Metadata> {
+  const user = await getUser(username);
+  return {
+    title: `${user?.name} (@${user?.username}) | Illustcomm Photos`,
+    description: `${user?.name}'s Ilustcomm posts`,
+  };
 }
