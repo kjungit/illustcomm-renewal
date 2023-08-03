e54ac0518729a133e6917443fdb1afeaccab6c20
kjun <kjun_all@naver.com>
2023-08-04 04:07:12 +0900

    Feat: post API Hook 구현

---
+++ src/hooks/usePost.tsx
@@ -0,0 +1,39 @@
+import { SimplePost } from "@/model/post";
+import useSWR, { useSWRConfig } from "swr";
+
+async function updateLike(id: string, like: boolean) {
+  return fetch("api/likes", {
+    method: "PUT",
+    body: JSON.stringify({ id, like }),
+  }).then((res) => res.json());
+}
+
+export default function usePosts() {
+  const {
+    data: posts,
+    isLoading,
+    error,
+    mutate,
+  } = useSWR<SimplePost[]>("/api/posts");
+
+  const setLike = (post: SimplePost, username: string, like: boolean) => {
+    const newPost = {
+      ...post,
+      likes: like
+        ? [...post.likes, username]
+        : post.likes.filter((item) => item !== username),
+    };
+
+    const newPosts = posts?.map((item) =>
+      item.id === post.id ? newPost : item
+    );
+
+    return mutate(updateLike(post.id, like), {
+      optimisticData: newPosts,
+      populateCache: false,
+      revalidate: false,
+      rollbackOnError: true,
+    });
+  };
+  return { posts, isLoading, error, setLike };
+}
