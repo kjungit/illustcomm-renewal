998a4eb75028027d84ab0ad6b9a95d6e07d10df7
kjun <kjun_all@naver.com>
2023-08-05 04:00:42 +0900

    Feat: useMe hook 연결

--- src/components/FollowingBar.tsx
+++ src/components/FollowingBar.tsx
@@ -1,20 +1,16 @@
 "use client";
 
-import { HomeUser } from "@/model/user";
 import Link from "next/link";
 import React from "react";
 import { SyncLoader } from "react-spinners";
-import useSWR from "swr";
 import Avatar from "./Avatar";
 import ScrollableBar from "./ui/ScrollableBar";
-export default function FollowingBar() {
-  const { data, isLoading, error } = useSWR<HomeUser>("/api/me");
+import useMe from "@/hooks/useMe";
 
-  const users = data?.following && [
-    ...data?.following,
-    ...data?.following,
-    ...data?.following,
-  ];
+export default function FollowingBar() {
+  const { user, isLoading, error } = useMe();
+
+  const users = user?.following;
 
   return (
     <section className="flex items-center justify-center w-full  p-4 mb-4 rounded-lg shadow-sm overflow-x-auto shadow-neutral-300 min-h-[120px] relative z-0">
