56b8f3b72771e2ef16364f0d7524fae481e81a72
kjun <kjun_all@naver.com>
2023-08-13 03:26:57 +0900

    Feat: 버튼 컴포넌트 props, style 수정

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -3,13 +3,25 @@ import React from "react";
 type Props = {
   text: string;
   onClick: () => void;
+  color?: boolean;
+  disabled?: boolean;
 };
 
-export default function Button({ text, onClick }: Props) {
+export default function Button({
+  text,
+  onClick,
+  color,
+  disabled = false,
+}: Props) {
   return (
     <button
-      className="p-2 font-bold leading-4 text-white bg-black border-none rounded-md dark:text-black dark:bg-white"
-      onClick={onClick}
+      className={`${
+        color ? "bg-blue-600" : "bg-black dark:text-black dark:bg-white"
+      } p-2 font-bold leading-4 text-white  border-none rounded-md ${
+        disabled && "opacity-60"
+      }`}
+      onClick={() => onClick()}
+      disabled={disabled}
     >
       {text}
     </button>
