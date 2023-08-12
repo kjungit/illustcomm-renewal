b510b9b2a411dd23a1b417621bf708b5eb494fc4
kjun <kjun_all@naver.com>
2023-08-13 02:46:53 +0900

    Feat: 상태추가 및 form submit 함수 구현

--- src/components/CommentForm.tsx
+++ src/components/CommentForm.tsx
@@ -1,19 +1,43 @@
+import { FormEvent, useState } from "react";
 import Avatar from "./Avatar";
 
 type Props = {
-  image: string;
+  image?: string;
+  onPostComment: (comment: string) => void;
 };
 
-export default function CommentForm({ image }: Props) {
+export default function CommentForm({ image, onPostComment }: Props) {
+  const [comment, setComment] = useState("");
+  const buttonDisabled = comment.length === 0;
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    onPostComment(comment);
+    setComment("");
+  };
+
   return (
-    <form className="flex items-center border-t border-neutral-300">
-      {image && <Avatar image={image} size="xsmall" highlight />}
+    <form
+      onSubmit={handleSubmit}
+      className="flex items-center px-4 border-t border-neutral-300"
+    >
+      <div>{image && <Avatar image={image} size="xsmall" highlight />}</div>
       <input
         className="w-full p-3 ml-2 border-none outline-none"
         type="text"
         placeholder="Add a comment..."
+        required
+        value={comment}
+        onChange={(e) => setComment(e.target.value)}
       />
-      <button className="ml-2 font-bold">Post</button>
+      <button
+        className={`ml-2 font-bold ${
+          buttonDisabled ? "opacity-40" : "opacity-100"
+        }`}
+        disabled={buttonDisabled}
+      >
+        Post
+      </button>
     </form>
   );
 }
