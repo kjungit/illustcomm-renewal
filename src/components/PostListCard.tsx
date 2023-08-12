a5ba14d7a049791a66484366e837e4cd54699b19
kjun <kjun_all@naver.com>
2023-08-13 03:22:52 +0900

    Feat: usePosts hook 연결 및 datail 모달 연결

--- src/components/PostListCard.tsx
+++ src/components/PostListCard.tsx
@@ -1,27 +1,27 @@
 "use client";
 
-import { SimplePost } from "@/model/post";
+import { Comment, SimplePost } from "@/model/post";
 import React, { useState } from "react";
-import Avatar from "./Avatar";
 import Image from "next/image";
-
-import { useSession } from "next-auth/react";
-import CommentForm from "./CommentForm";
 import CardInfoBar from "./CardInfoBar";
 import ModalPortal from "./ui/ModalPortal";
 import PostModal from "./ui/PostModal";
 import PostDetail from "./PostDetail";
 import PostUserAvatar from "./PostUserAvatar";
+import usePosts from "@/hooks/usePosts";
 type Props = {
   post: SimplePost;
   priority?: boolean;
 };
 
 export default function PostListCard({ post, priority = false }: Props) {
-  const { userImage, username, image, createdAt, likes, text } = post;
+  const { userImage, username, image, comments, text } = post;
   const [openModal, setOpenModal] = useState(false);
-  const { data: session } = useSession();
-  const user = session?.user;
+  const { postComment } = usePosts();
+  const handlePostComment = (comment: Comment) => {
+    postComment(post, comment);
+  };
+
   return (
     <article className="border border-gray-200 rounded-lg shadow-md">
       <PostUserAvatar username={username} image={userImage} />
@@ -34,7 +34,18 @@ export default function PostListCard({ post, priority = false }: Props) {
         priority={priority}
         onClick={() => setOpenModal(true)}
       />
-      <CardInfoBar post={post} myProfile={user?.image} />
+      <CardInfoBar post={post} onComment={handlePostComment}>
+        <div className="flex text-sm">
+          <span className="font-bold">{username}</span>
+          <p className="ml-2">{text}</p>
+        </div>
+        {comments > 1 && (
+          <button
+            className="text-sm "
+            onClick={() => setOpenModal(true)}
+          >{`댓글 ${comments}개 모두 보기`}</button>
+        )}
+      </CardInfoBar>
       {openModal && (
         <ModalPortal>
           <PostModal onClose={() => setOpenModal(false)}>
