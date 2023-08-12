031492ea24deeda1105bf77d988352f12cb00fb1
kjun <kjun_all@naver.com>
2023-08-13 02:52:25 +0900

    Feat: API연결 및 로딩 및 데이터 fetching - ssr 변경된 데이터 refresh

--- src/components/FollowButton.tsx
+++ src/components/FollowButton.tsx
@@ -1,8 +1,10 @@
 "use client";
 import { ProfileUser } from "@/model/user";
-import React from "react";
+import React, { useState, useTransition } from "react";
 import Button from "./ui/Button";
 import useMe from "@/hooks/useMe";
+import { useRouter } from "next/navigation";
+import { PulseLoader } from "react-spinners";
 
 type Props = {
   user: ProfileUser;
@@ -10,18 +12,45 @@ type Props = {
 
 export default function FollowButton({ user }: Props) {
   const { username } = user;
-  const { user: loggedInUser } = useMe();
+  const { user: loggedInUser, toggleFollow } = useMe();
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const [isFetching, setIsFetching] = useState(false);
+  const isUpdating = isPending || isFetching;
 
   const showButton = loggedInUser && loggedInUser.username !== username;
   const isFollowing =
     loggedInUser &&
     loggedInUser.following.find((item) => item.username === username);
 
-  const text = isFollowing ? "unFollow" : "Follow";
+  const text = isFollowing ? "UnFollow" : "Follow";
 
-  const onClick = () => {
-    console.log("click");
+  const handleFollow = async () => {
+    setIsFetching(true);
+    await toggleFollow(user.id, !isFollowing);
+    setIsFetching(false);
+    startTransition(() => {
+      router.refresh();
+    });
   };
 
-  return <>{showButton && <Button text={text} onClick={onClick} />}</>;
+  return (
+    <>
+      {showButton && (
+        <div className="relative">
+          {isUpdating && (
+            <div className="absolute inset-0 z-20 flex items-center justify-center">
+              <PulseLoader size={6} />
+            </div>
+          )}
+          <Button
+            disabled={isUpdating}
+            text={text}
+            onClick={handleFollow}
+            color={text === "Follow"}
+          />
+        </div>
+      )}
+    </>
+  );
 }
