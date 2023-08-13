98a50cc6d69651dc3f6e89bd14f81121e9026e49
kjun <kjun_all@naver.com>
2023-08-13 22:30:04 +0900

    Refactor: withSessionUser 모듈 변환

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,38 +1,27 @@
-import { authOptions } from "@/lib/auth";
 import { createPost, getFollowingPostOf } from "@/service/posts";
-import { getServerSession } from "next-auth";
+import { withSessionUser } from "@/utils/session";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
-  const session = await getServerSession(authOptions);
-  const user = session?.user;
-
-  if (!user) {
-    return new Response("Authentication Error", { status: 401 });
-  }
-
-  return getFollowingPostOf(user.username).then((data) =>
-    NextResponse.json(data)
-  );
+  return withSessionUser(async (user) => {
+    return getFollowingPostOf(user.username).then((data) =>
+      NextResponse.json(data)
+    );
+  });
 }
 
 export async function POST(req: NextRequest) {
-  const session = await getServerSession(authOptions);
-  const user = session?.user;
+  return withSessionUser(async (user) => {
+    const form = await req.formData();
+    const text = form.get("text")?.toString();
+    const file = form.get("file") as Blob;
 
-  if (!user) {
-    return new Response("Authentication Error", { status: 401 });
-  }
+    if (!text || !file === undefined) {
+      return new Response("Bad Request", { status: 400 });
+    }
 
-  const form = await req.formData();
-  const text = form.get("text")?.toString();
-  const file = form.get("file") as Blob;
-
-  if (!text || !file === undefined) {
-    return new Response("Bad Request", { status: 400 });
-  }
-
-  return createPost(user.id, text, file).then((data) =>
-    NextResponse.json(data)
-  );
+    return createPost(user.id, text, file).then((data) =>
+      NextResponse.json(data)
+    );
+  });
 }
