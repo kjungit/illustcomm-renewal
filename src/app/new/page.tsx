bdda64f364afcf4e2c07c38a26586a1f519eccd4
kjun <kjun_all@naver.com>
2023-08-13 21:35:18 +0900

    Feat: 글 작성 페이지 구현 - metadata 작성 - NewPost 컴포넌트 연결

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,5 +1,20 @@
+import NewPost from "@/components/NewPost";
+import { authOptions } from "@/lib/auth";
+import { Metadata } from "next";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 import React from "react";
 
-export default function NewPostPage() {
-  return <div>NewPostPage</div>;
+export const metadata: Metadata = {
+  title: "새로운 작품",
+  description: "새로운 작품을 등록하세요.",
+};
+
+export default async function NewPostPage() {
+  const session = await getServerSession(authOptions);
+  if (!session?.user) {
+    redirect("/auth/signin");
+  }
+
+  return <NewPost user={session.user} />;
 }
