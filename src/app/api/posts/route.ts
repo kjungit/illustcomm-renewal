0712d18095b52fe5e4f54379ab9d0c1b06341421
kjun <kjun_all@naver.com>
2023-08-24 01:17:20 +0900

    Feat: post 수정 route 코드 추가

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,4 +1,4 @@
-import { createPost, getFollowingPostOf } from "@/service/posts";
+import { createPost, getFollowingPostOf, updatePost } from "@/service/posts";
 import { withSessionUser } from "@/utils/session";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -23,5 +23,26 @@ export async function POST(req: NextRequest) {
     return createPost(user.id, text, file).then((data) =>
       NextResponse.json(data)
     );
+  });
+}
+
+export async function PUT(req: NextRequest) {
+  return withSessionUser(async (user) => {
+    const form = await req.formData();
+    const postId = form.get("postId")?.toString();
+    const newText = form.get("newText")?.toString();
+    const newFile = form.get("newFile") as Blob;
+
+    if (!postId || !newText || newFile === undefined) {
+      return new Response("Bad Request", { status: 400 });
+    }
+
+    try {
+      await updatePost(user.id, postId, newText, newFile);
+      return NextResponse.json({ success: true });
+    } catch (error) {
+      console.error(error);
+      return new Response("Internal Server Error", { status: 500 });
+    }
   });
 }
