1d69367d78f7b6a7b850ad09f2c34fbacc42417d
kjun <kjun_all@naver.com>
2023-08-24 01:16:08 +0900

    Feat: post 삭제 route 추가

--- src/app/api/posts/[id]/route.ts
+++ src/app/api/posts/[id]/route.ts
@@ -1,4 +1,4 @@
-import { getPost } from "@/service/posts";
+import { deletePost, getPost } from "@/service/posts";
 import { withSessionUser } from "@/utils/session";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -9,5 +9,23 @@ type Context = {
 export async function GET(_: NextRequest, context: Context) {
   return withSessionUser(async () => {
     return getPost(context.params.id).then((data) => NextResponse.json(data));
+  });
+}
+
+export async function DELETE(_: NextRequest, context: Context) {
+  return withSessionUser(async (user) => {
+    const postId = context.params.id;
+
+    if (!postId) {
+      return new Response("Bad Request", { status: 400 });
+    }
+
+    try {
+      await deletePost(postId);
+      return NextResponse.json({ success: true });
+    } catch (error) {
+      console.error(error);
+      return new Response("Internal Server Error", { status: 500 });
+    }
   });
 }
