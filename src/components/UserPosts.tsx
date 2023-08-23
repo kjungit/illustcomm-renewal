abe24610aa5263f7499a3570586fb26b162d8c30
kjun <kjun_all@naver.com>
2023-08-24 01:47:16 +0900

    Feat: aria-label 추가

--- src/components/UserPosts.tsx
+++ src/components/UserPosts.tsx
@@ -11,9 +11,17 @@ type Props = {
 };
 
 const tabs = [
-  { type: "posts", icon: <PostIcon /> },
-  { type: "saved", icon: <BookmarkIcon className="w-3 h-3" /> },
-  { type: "liked", icon: <HeartIcon className="w-3 h-3" /> },
+  { type: "posts", title: "User posts", icon: <PostIcon /> },
+  {
+    type: "saved",
+    title: "Saved posts",
+    icon: <BookmarkIcon className="w-3 h-3" />,
+  },
+  {
+    type: "liked",
+    title: "Liked posts",
+    icon: <HeartIcon className="w-3 h-3" />,
+  },
 ];
 
 export default function UserPosts({ user: { username } }: Props) {
@@ -22,7 +30,7 @@ export default function UserPosts({ user: { username } }: Props) {
   return (
     <section>
       <ul className="flex justify-center uppercase">
-        {tabs.map(({ type, icon }) => (
+        {tabs.map(({ type, icon, title }) => (
           <li
             className={`p-4 mx-12 cursor-pointer border-black dark:border-white ${
               type === query && "font-bold border-t-2"
@@ -30,7 +38,9 @@ export default function UserPosts({ user: { username } }: Props) {
             key={type}
             onClick={() => setQuery(type)}
           >
-            <button className="mr-2 scale-150 md:scale-120">{icon}</button>
+            <button className="mr-2 scale-150 md:scale-120" aria-label={title}>
+              {icon}
+            </button>
             <span className="hidden md:inline">{type}</span>
           </li>
         ))}
