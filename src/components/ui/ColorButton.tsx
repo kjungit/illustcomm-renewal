c3038a74e93c495c302144adce5779e8ebc22b47
kjun <kjun_all@naver.com>
2023-09-02 01:47:34 +0900

    UI: 스타일 코드 수정

--- src/components/ui/ColorButton.tsx
+++ src/components/ui/ColorButton.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export default function ColorButton({ text, onClick }: Props) {
   return (
-    <div className="p-[0.15rem] rounded-md bg-gradient-to-bl from-gray-600 via-gray-300 to-gray-100 ">
+    <div className="p-[0.15rem] rounded-md bg-gradient-to-bl ">
       <button
         className="p-1 font-medium transition-opacity bg-white rounded-md hover:opacity-80 dark:text-black"
         onClick={onClick}
